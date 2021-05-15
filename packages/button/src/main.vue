07d680b3d4c44b485bed094563dd410d6932db7c
coderldt <lvchao@jianjukeji.com>
2021-05-15 22:19:01 +0800

    fix: button init

--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="p-button">
-    <button></button>
+  <div 
+    class="p-button"
+    :class="
+      [
+        type ? `p-button-${type}` : '',
+        size ? `p-button-${size}` : '',
+        disabled ? `p-button-disabled` : '',
+        circle ? `p-button-circle` : '',
+        text ? `p-button-text` : ''
+      ]
+    "
+  >
+    <button @click="handlyClick"><slot></slot></button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PButton'
+  name: 'PButton',
+  props: {
+    type: {
+      type: String,
+      default: '',
+      validator: val => ['yellow', 'green', 'red', 'blue', 'gray', 'pepper'].includes(val)
+    },
+    size: {
+      type: String,
+      default: 'm',
+      validator: val => ['s', 'm', 'l'].includes(val)
+    },
+    disabled: Boolean,
+    circle: Boolean,
+    text: Boolean
+  },
+  methods: {
+    handlyClick (e) {
+      this.$emit('click', e)
+    }
+  }
 }
 </script>
--- test/unit/specs/button.spec.js
+++ test/unit/specs/button.spec.js
@@ -1,4 +1,4 @@
-import { createTest, destroyVM } from '../util';
+import { createTest, createVue, destroyVM } from '../util';
 import Button from 'packages/button';
 
 describe('Button', () => {
@@ -11,5 +11,65 @@ describe('Button', () => {
     vm = createTest(Button, true);
     expect(vm.$el).to.exist;
   })
+
+  it('size', () => {
+    vm = createTest(Button, {
+      size: 'l'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('p-button-l')).to.be.true;
+  });
+
+  it('type', () => {
+    vm = createTest(Button, {
+      size: 'pepper'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('p-button-pepper')).to.be.true;
+  });
+
+  it('disabled', () => {
+    vm = createTest(Button, {
+      disabled: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('p-button-disabled')).to.be.true;
+  });
+
+  it('circle', () => {
+    vm = createTest(Button, {
+      circle: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('p-button-circle')).to.be.true;
+  });
+
+  it('text', () => {
+    vm = createTest(Button, {
+      text: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('p-button-text')).to.be.true;
+  });
+
+  it('click', done => {
+    let result;
+    vm = createVue({
+      template: `
+        <p-button @click="handleClick"></p-button>
+      `,
+      methods: {
+        handleClick(evt) {
+          result = evt;
+        }
+      }
+    }, true);
+    vm.$el.click();
+
+    setTimeout(() => {
+      expect(result).to.exist;
+      done();
+    }, 20);
+  });
 });
 
